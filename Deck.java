81892e25eacfa24211f855365e939a32a327ed56
lleone201 <lleone201@gmail.com>
2020-04-12 21:27:49 -0400

    Finished the deck class

--- Deck.java
+++ Deck.java
@@ -1,4 +1,49 @@
+import java.util.ArrayList;
+import java.util.Collections;
 
 public class Deck {
+    // Deck wil need cards obviously
+    ArrayList<Card> cards;
+
+    // Deck constructor
+    public Deck() {
+        // Create 52 cards for the deck and shuffle them
+        cards = new ArrayList<Card>();
+        String suit = "";
+        for (int i = 0; i < 4; ++i) {
+            if (i == 0) {
+                suit = "hearts";
+            } else if (i == 1) {
+                suit = "clubs";
+            } else if (i == 2) {
+                suit = "spades";
+            } else if (i == 4) {
+                suit = "diamonds";
+            }
+            for (int j = 2; j <= 14; ++i) {
+                cards.add(new Card(suit, i));
+            }
+        }
+        cards.shuffleDeck();
+    }
+
+    public int getDeckSize() {
+        // Returns the current number of cards left in the deck
+        return cards.size();
+    }
+
+    public void shuffleDeck() {
+        // Uses the Collections.shuffle method to shuffle up the deck and get the cards
+        // in randomized order
+        Collections.shuffle(cards);
+    }
+
+    public Card drawCard() {
+        // Draws a card from the deck and then removes it from the cards in the deck.
+        Card drawn = cards.get(0);
+        cards.remove(0);
+
+        return drawn;
+    }
 
 }
